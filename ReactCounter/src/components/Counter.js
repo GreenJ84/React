b937f7bd5b38b2c7059e6b6de374f478fa06860d
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-24 15:33:48 -0800

    Added classNames to counter

--- ReactCounter/src/components/Counter.js
+++ ReactCounter/src/components/Counter.js
@@ -1,10 +1,12 @@
+import classes from './Counter.module.css';
+
 const Counter = () => {
   const toggleCounterHandler = () => {};
 
   return (
-    <main className={}>
+    <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>-- COUNTER VALUE --</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
