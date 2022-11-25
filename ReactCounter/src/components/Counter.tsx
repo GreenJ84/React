99e8b3068dbad41c1ef25f25499993d408f2352b
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-24 23:35:08 -0800

    removed interface and imported instead

--- ReactCounter/src/components/Counter.tsx
+++ ReactCounter/src/components/Counter.tsx
@@ -1,16 +1,11 @@
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./Counter.module.css";
-import { decrement, increment, incrementByAmount } from '../slices/counterSlice'
-
-
-interface counterType{
-  counter: { value: number}
-}
+import { decrement, increment, incrementByAmount, counterType } from '../slices/counterSlice'
 
 const Counter = () => {
   const [toggle, setToggle] = useState(false)
-  const counter = useSelector((state: counterType)  => state.counter.value);
+  const counter = useSelector((state: counterType)  => state.value);
   const dispatch = useDispatch();
 
   const toggleCounterHandler = () => {
