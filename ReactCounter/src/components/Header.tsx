fb33232d73b3344f9e22c68f8d7ade4b72c92a36
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-24 23:28:29 -0800

    Added function to header buttons

--- ReactCounter/src/components/Header.tsx
+++ ReactCounter/src/components/Header.tsx
@@ -1,6 +1,24 @@
 import './Header.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, toggleForm } from '../slices/authSlice'
+
+interface authType {
+  isAuthenticated: boolean
+  needsLogin: boolean
+}
 
 const Header = () => {
+  const loggedIn = useSelector((state: authType) => state.isAuthenticated);
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
+  const toggleLoginForm = () => {
+    dispatch(toggleForm())
+  }
+
   return (
     <header>
       <h1>Redux Auth</h1>
@@ -13,7 +31,14 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Login</button>
+            {loggedIn ? 
+            <button onClick={ logoutHandler }>
+              Logout
+            </button>:
+            <button onClick={ toggleLoginForm }>
+              Login
+            </button>
+            }
           </li>
         </ul>
       </nav>
