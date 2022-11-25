d8893d152bc6c984552473407538ad40de229232
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-24 21:28:55 -0800

    Updated and finalized app/store

--- ReactCounter/src/app/store.ts
+++ ReactCounter/src/app/store.ts
@@ -1,12 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../slices/counterSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../slices/counterSlice";
 
 const store = configureStore({
     reducer: {
         counter: counterReducer,
-    }
+    },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
+export default store;
