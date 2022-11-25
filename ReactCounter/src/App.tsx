e8268b97821cf19081d6103bf9fad7a63fe62843
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-24 23:43:58 -0800

    Updated app to display base on authentication

--- ReactCounter/src/App.tsx
+++ ReactCounter/src/App.tsx
@@ -2,12 +2,20 @@ import React from 'react'
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from './components/Auth'
+import UserProfile from './components/UserProfile';
+import { useSelector } from 'react-redux';
+import { authType } from './slices/authSlice';
 
 
 const App = () => {
+  const loggedIn = useSelector((state: authType) => state.isAuthenticated);
+  const displayForm = useSelector((state: authType) => state.loginForm);
+
+
   return (<>
     <Header />
-    <Auth />
+    {loggedIn ? <UserProfile /> : 
+    displayForm? <Auth /> : ''}
     <Counter />
     </>
   );
