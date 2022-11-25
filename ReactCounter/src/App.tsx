0410de63b854cad8b94da7f811739cd7908a9358
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-24 23:11:55 -0800

    Added authentication component to app

--- ReactCounter/src/App.tsx
+++ ReactCounter/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Counter from "./components/Counter";
 import Header from "./components/Header";
+import Auth from './components/Auth'
 
 
 const App = () => {
   return (<>
     <Header />
+    <Auth />
     <Counter />
     </>
   );
