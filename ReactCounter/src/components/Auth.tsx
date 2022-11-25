56cbde240cc34d7e6d3054fd464f3fa2ac9f3c4e
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-24 23:04:06 -0800

    Created an authentication form

---
+++ ReactCounter/src/components/Auth.tsx
@@ -0,0 +1,23 @@
+import './Auth.module.css';
+
+const Auth = () => {
+  return (
+    <main>
+      <section>
+        <form>
+          <div>
+            <label htmlFor='email'>Email</label>
+            <input type='email' id='email' />
+          </div>
+          <div>
+            <label htmlFor='password'>Password</label>
+            <input type='password' id='password' />
+          </div>
+          <button>Login</button>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default Auth;
