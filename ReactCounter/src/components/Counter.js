0ce4e6c3359dfdf6aa5feae5fb4d7aeeac16674a
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-24 21:49:45 -0800

    Added counter, increment, and decrement

--- ReactCounter/src/components/Counter.js
+++ ReactCounter/src/components/Counter.js
@@ -1,18 +1,39 @@
-import { useSelector, useDispatch } from 'react-redux'
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux";
+import classes from "./Counter.module.css";
+import { decrement, increment } from '../slices/counterSlice'
 
 const Counter = () => {
-  const counter = useSelector(state => state.counter.value)
-  const dispatch = useDispatch()
+  const counter = useSelector(state => state.counter.value);
+  const dispatch = useDispatch();
 
   const toggleCounterHandler = () => {};
 
+  const incrementCounterHandler = () => {
+    dispatch(increment());
+  };
+
+  const decrementCounterHandler = () => {
+    dispatch(decrement())
+  };
+
   return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>{ counter }</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
+      <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div className={classes.value}>
+          { counter }
+        </div>
+        <div>
+          <button onClick={incrementCounterHandler}>
+            Increment
+          </button>
+          <button onClick={decrementCounterHandler}>
+            Decrement
+          </button>
+        </div>
+        <button onClick={toggleCounterHandler}>
+          Toggle Counter
+        </button>
+      </main>
   );
 };
 
