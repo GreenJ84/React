734929d205e6400673005ef7fbe8cbec8cb2bc32
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-24 22:59:10 -0800

    Added fragment to enclose coponents with Header

--- ReactCounter/src/App.tsx
+++ ReactCounter/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Counter from "./components/Counter";
+import Header from "./components/Header";
 
 
 const App = () => {
-  return (
+  return (<>
+    <Header />
     <Counter />
+    </>
   );
 }
 
