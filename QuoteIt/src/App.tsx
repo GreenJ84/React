55ff9f7d04ac84097bccff856f7c3fa513eff402
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-26 00:44:57 -0800

    Import and implement Layout base component

--- QuoteIt/src/App.tsx
+++ QuoteIt/src/App.tsx
@@ -6,18 +6,19 @@ import NewQuote from './views/pages/NewQuote'
 import QuoteDetail from './views/pages/QuoteDetail'
 import NotFound from './views/pages/NotFound'
 import NoQuotesFound from "./views/components/quotes/NoQuotesFound";
+import Layout from "./views/components/layout/Layout";
 
 function App() {
   return (
-
-    <Routes >
-      <Route path='/' element={<NoQuotesFound />}/>
-      <Route path='/quotes' element={<AllQuotes />}/>
-      <Route path='/quotes/:quoteId' element={<QuoteDetail />}/>
-      <Route path='/newQuote' element={<NewQuote />}/>
-      <Route path='*' element={<NotFound />}/>
-    </Routes>
-
+      <Layout >
+        <Routes >
+          <Route path='/' element={<NoQuotesFound />}/>
+          <Route path='/quotes' element={<AllQuotes />}/>
+          <Route path='/quotes/:quoteId' element={<QuoteDetail />}/>
+          <Route path='/newQuote' element={<NewQuote />}/>
+          <Route path='*' element={<NotFound />}/>
+        </Routes>
+      </Layout>
   );
 }
 
