c7b05328d4803e36ede6c421cec6e1e0f9f73375
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-25 22:47:02 -0800

    Create New Quote component with from child

--- QuoteIt/src/views/pages/NewQuote.tsx
+++ QuoteIt/src/views/pages/NewQuote.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { newQuote, quoteType } from '../../app/slices/quoteSlice'
 
 const NewQuote = () => {
+    const dispatch = useDispatch()
+
+    const newQuotehandler = ( data: quoteType ) => {
+        dispatch(newQuote(data))
+    }
+
     return (
-        <div>NewQuote</div>
+        <>
+        {/* Display a Quote Form <QuoteForm callback={ newQuoteHandler } */}
+        </>
     )
 }
 
-export default NewQuote
+export default NewQuote;
