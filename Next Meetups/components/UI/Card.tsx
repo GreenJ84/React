9ff1cf1cc07682a7304245ef2961ac8832d90298
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-26 14:10:31 -0800

    Added styling to Card

--- Next Meetups/components/UI/Card.tsx
+++ Next Meetups/components/UI/Card.tsx
@@ -12,7 +12,9 @@ const Card = (props: cardProps) => {
 }
 
 const StyledCard = styled.div`
-
+    background-color: white;
+    border-radius: 6px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
 `;
 
 export default Card;
