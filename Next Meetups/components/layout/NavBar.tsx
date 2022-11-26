9f5ef9ad6268082492091905af12ba2dea239c26
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-26 15:32:03 -0800

    Created NavBar component

---
+++ Next Meetups/components/layout/NavBar.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import React from 'react'
+
+const NavBar = () => {
+    
+    return (
+        <header>
+            <div>React Meetups</div>
+            <nav>
+                <ul>
+                <li>
+                    <Link href='/'>
+                            All Meetups
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/new-meetup'>
+                        Add New Meetup
+                    </Link>
+                </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default NavBar;
