9439391273bb1711be30282994b7cc1f74b942ba
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-25 23:03:04 -0800

    Added No quotes found to main route

--- QuoteIt/src/App.tsx
+++ QuoteIt/src/App.tsx
@@ -5,11 +5,13 @@ import AllQuotes from './views/pages/AllQuotes'
 import NewQuote from './views/pages/NewQuote'
 import QuoteDetail from './views/pages/QuoteDetail'
 import NotFound from './views/pages/NotFound'
+import NoQuotesFound from "./views/components/quotes/NoQuotesFound";
 
 function App() {
   return (
 
     <Routes >
+      <Route path='/' element={<NoQuotesFound />}/>
       <Route path='/quotes' element={<AllQuotes />}/>
       <Route path='/quotes/:quoteId' element={<QuoteDetail />}/>
       <Route path='/newQuote' element={<NewQuote />}/>
