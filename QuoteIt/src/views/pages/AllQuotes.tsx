10ca67d886ca7c713d494b74f615c51c2611bb2b
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-25 22:22:03 -0800

    Use store to dynamically render

--- QuoteIt/src/views/pages/AllQuotes.tsx
+++ QuoteIt/src/views/pages/AllQuotes.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { storeType } from '../../app/store'
 
 const AllQuotes = () => {
+    const quotes = useSelector((state: storeType ) => state.quotes)
+
+    if (quotes.length < 1){
+        return (
+            null
+            // <NoQuotesFound />
+        )
+    }
+
     return (
-        <div>AllQuotes</div>
+        null
+        // <QuoteList />
     )
 }
 
