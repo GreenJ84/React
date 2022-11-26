dac5cb84f2e738d7e7e09817e8103f92cc618a54
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-26 02:34:11 -0800

    Created QuoteDetails component

--- QuoteIt/src/views/pages/QuoteDetail.tsx
+++ QuoteIt/src/views/pages/QuoteDetail.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Outlet, useParams } from 'react-router-dom'
+import { quoteType } from '../../app/slices/quoteSlice'
+import { storeType } from '../../app/store'
+import HighlightedQuote from '../components/quotes/HighlightedQuote'
 
 const QuoteDetail = () => {
+    const quotes = useSelector((state: storeType) => state.quotes)
+    const [thisQuote, setThisQuote] = useState<quoteType>()
+    const params = useParams()
+    const { quoteId } = params
+
+    
+    useEffect(()=> {
+        if (quoteId){
+            setThisQuote(quotes.filter(quote => quote.id === parseInt(quoteId))[0])
+        }
+    }, [])
+    
+
     return (
-        <div>QuoteDetail</div>
+        <>
+            {thisQuote && <HighlightedQuote text={thisQuote.text} author={thisQuote.author} />}
+            <Outlet />
+        </>
     )
 }
 
