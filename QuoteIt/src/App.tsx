8451d2d19c54bb26aaa56e716f7be8896d04ca8b
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-25 21:24:55 -0800

    Added Routes templates

--- QuoteIt/src/App.tsx
+++ QuoteIt/src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <>
+    <Route />{/* All Quotes Home - <AllQuotes /> */} 
+    <Route />{/* Quote Detail - <QuoteDetail /> */}
+    <Route />{/* New Quote - <NewQuote /> */}
+    <Route />{/* Not Found - <NotFound /> */}
     <h1>New React-Redux</h1>
     </>
   );
