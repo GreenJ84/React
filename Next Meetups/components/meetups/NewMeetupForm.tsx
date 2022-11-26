b1b4b83dcde0a963157745a8605e6db87758fcf2
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-26 15:17:23 -0800

    created a NewMeetupForm component

---
+++ Next Meetups/components/meetups/NewMeetupForm.tsx
@@ -0,0 +1,70 @@
+import React, { useRef } from 'react'
+import Card from '../ui/Card'
+
+interface meetupFormProps {
+    onAddMeetup: Function
+}
+
+const NewMeetupForm = (props: meetupFormProps) => {
+    const titleInputRef = useRef<HTMLInputElement>(null);
+    const imageInputRef = useRef<HTMLInputElement>(null);
+    const addressInputRef = useRef<HTMLInputElement>(null);
+    const descriptionInputRef = useRef<HTMLTextAreaElement>(null);
+    
+    function submitHandler(event: React.FormEvent) {
+        event.preventDefault();
+    
+        const enteredTitle = titleInputRef.current?.value;
+        const enteredImage = imageInputRef.current?.value;
+        const enteredAddress = addressInputRef.current?.value;
+        const enteredDescription = descriptionInputRef.current?.value;
+    
+        const meetupData = {
+            title: enteredTitle,
+            image: enteredImage,
+            address: enteredAddress,
+            description: enteredDescription,
+        };
+    
+        props.onAddMeetup(meetupData);
+        }
+    return (
+        <Card>
+            <form onSubmit={submitHandler}>
+                <div>
+                    <label htmlFor='title'>
+                        Meetup Title
+                    </label>
+                    <input type='text' required id='title' ref={titleInputRef} />
+                </div>
+                <div>
+                    <label htmlFor='image'>
+                        Meetup Image
+                    </label>
+                    <input type='url' required id='image' ref={imageInputRef} />
+                </div>
+                <div>
+                    <label htmlFor='address'>
+                            Address
+                    </label>
+                    <input type='text' required id='address' ref={addressInputRef} />
+                </div>
+                <div>
+                    <label htmlFor='description'>
+                        Description
+                    </label>
+                    <textarea id='description' required rows={ 5 } ref={descriptionInputRef}
+                        >
+                    </textarea>
+                </div>
+                <div>
+                    <button>
+                        Add Meetup
+                    </button>
+                </div>
+            </form>
+        </Card>
+    )
+}
+
+export default NewMeetupForm
