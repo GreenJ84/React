16fd2dec6e6512fb225a8124f05e37f2bdacc283
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-27 15:24:59 -0800

    Get detail DUMMY-DATA working

--- Next Meetups/pages/[meetupId]/index.tsx
+++ Next Meetups/pages/[meetupId]/index.tsx
@@ -1,17 +1,47 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { ParsedUrlQuery } from 'querystring';
 import React from 'react'
+import { DUMMY_DATA } from '..';
 import MeetupDetail, { meetupDetailProps } from '../../components/meetups/MeetupDetail';
 
 const DetailPage = (props: meetupDetailProps) => {
 
     return (
         <>
-        <MeetupDetail id={1}
+        <MeetupDetail id={ props.id }
         image={props.image}
         title={props.title}
         address={props.address}
         description={props.description}/>
         </>
     )
+}
+
+interface Params extends ParsedUrlQuery {
+    meetupId: string
+}
+
+export const getStaticPaths: GetStaticPaths<Params>= async () => {
+
+    return {
+        fallback: false,
+        paths: [{
+            params: {
+                meetupId: '1'
+            },
+        }]
+    }
+}
+
+export const getStaticProps: GetStaticProps<meetupDetailProps, Params> = async (context) => {
+
+    const { meetupId } = context.params!;
+    
+    const x = DUMMY_DATA.filter(data => data.id === parseInt(meetupId))[0] as meetupDetailProps;
+
+    return {
+        props: x,
+    }
 }
 
 export default DetailPage
