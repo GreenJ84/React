311f7af9a1909dbeffd15a1b1a3973fa7a676b2f
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-27 15:24:25 -0800

    Finalize NewMeetup comp with api call

--- Next Meetups/pages/new-meetup/index.tsx
+++ Next Meetups/pages/new-meetup/index.tsx
@@ -1,10 +1,32 @@
 import { Head } from 'next/document';
+import Router, { useRouter } from 'next/router';
 import React from 'react'
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
-const NewMeetup = () => {
+interface NewData{
+    title: string
+    author: string
+    image: string
+    description: string
+}
 
-    const onAddMeetup = () => {}
+const NewMeetup = () => {
+    const router = useRouter()
+
+    const onAddMeetup = async (enteredData: NewData) => {
+        const response = await fetch('/api/new-meetup', {
+            method: 'POST',
+            body: JSON.stringify(enteredData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json()
+
+        console.log(data)
+
+        router.replace('/')
+    }
 
     return (
         <>
